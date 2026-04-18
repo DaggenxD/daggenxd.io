69b8ad4e81a5e6cbc9018b8104db4f2c5a0c9875
DaggenxD <danilogframos@gmail.com>
2026-04-18 15:54:06 -0300

    Update video sources and add new video planes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //initiate the AR 3 object
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/targets_new.mind'
+      imageTargetSrc: './assets/targets/targets_3_photos.mind'
     });
     const {renderer, scene, camera} = mindarThree;
 
@@ -22,11 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const material1 = new THREE.MeshBasicMaterial({map: texture1});
   const plane1 = new THREE.Mesh(geometry1, material1);
 
-  //const video2 = await loadVideo("./assets/videos/Doctor scary face.mp4");
-  //const texture2 = new THREE.VideoTexture(video2);
-  //const geometry2 = new THREE.PlaneGeometry(1, 240/428);
-  //const material2 = new THREE.MeshBasicMaterial({map: texture2});
-  //const plane2 = new THREE.Mesh(geometry2, material2);
+  const video2 = await loadVideo("./assets/videos/Video Cena Lazer.mp4");
+  const texture2 = new THREE.VideoTexture(video2);
+  const geometry2 = new THREE.PlaneGeometry(1, 240/428);
+  const material2 = new THREE.MeshBasicMaterial({map: texture2});
+  const plane2 = new THREE.Mesh(geometry2, material2);
+
+  const video3 = await loadVideo("./assets/videos/Video Cena Frontal - Compress.mp4");
+  const texture3 = new THREE.VideoTexture(video3);
+  const geometry3 = new THREE.PlaneGeometry(1, 240/428);
+  const material3 = new THREE.MeshBasicMaterial({map: texture3});
+  const plane3 = new THREE.Mesh(geometry3, material3);
 
 
   // add the first video plane to an anchor
@@ -41,17 +47,30 @@ document.addEventListener('DOMContentLoaded', () => {
     video1.pause();
   }
    
-  // add the second video plane to an anchor
-  //const anchor2 = mindarThree.addAnchor(1);
- // anchor2.group.add(plane2);
+   //add the second video plane to an anchor
+  const anchor2 = mindarThree.addAnchor(1);
+  anchor2.group.add(plane2);
 
-  //anchor2.onTargetFound = () => {
-   // //console.log('Video 2 started');
-   // video2.play();
-//  }
-  //anchor2.onTargetLost = () => {
- //   video2.pause();
-//  }
+  anchor2.onTargetFound = () => {
+    //console.log('Video 2 started');
+    video2.play();
+  }
+  anchor2.onTargetLost = () => {
+    video2.pause();
+  }
+
+   //add the third video plane to an anchor
+  const anchor3 = mindarThree.addAnchor(1);
+  anchor3.group.add(plane3);
+
+  anchor3.onTargetFound = () => {
+    //console.log('Video 2 started');
+    video3.play();
+  }
+  anchor3.onTargetLost = () => {
+    video3.pause();
+  }
+    
 //start the experience
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
